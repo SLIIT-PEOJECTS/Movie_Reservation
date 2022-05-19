ec38cca07559281ac546214867ea0de7ae1fcbfb
VisharaPrabuddhi <86484671+VisharaPrabuddhi@users.noreply.github.com>
2022-05-19 19:10:16 +0530

    session model

--- Frontend/movie-reservation/src/pages/SessionManager/AddSession.js
+++ Frontend/movie-reservation/src/pages/SessionManager/AddSession.js
@@ -1,99 +1,104 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 // import Select from 'react-select';
-import Navbar from '../../components/dashboard/Navbar';
-import Sidebar from '../../components/dashboard/Sidebar';
+import Navbar from "../../components/dashboard/Navbar";
+import Sidebar from "../../components/dashboard/Sidebar";
 // import firebase from './firebase';
 // import 'firebase/storage'
 
+
 const AddSession = () => {
+  //const [file, setFile] = useState(null);
+  //const [movieURL, setURL] = useState("");
+  //const [cast, setSelectedCast] = useState("");
+  //const [tags, setSelectedTag] = useState("");
 
-    //const [file, setFile] = useState(null);
-    //const [movieURL, setURL] = useState("");
-    //const [cast, setSelectedCast] = useState("");
-    //const [tags, setSelectedTag] = useState("");
+  const [state, setState] = useState({
+    sessionName: "",
+    fromTime: "",
+    toTime: "",
+  });
 
-    const [state, setState] = useState({
-        sessionName: "",
-        fromTime: "",
-        toTime: "",
+  const { sessionName, fromTime, toTime } = state;
+
+  function handleChange(name) {
+    return function (event) {
+      setState({ ...state, [name]: event.target.value });
+    };
+  }
+
+
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.table({
+      sessionName,
+      fromTime,
+      toTime,
+      // close,
     });
 
-    const {
+    axios
+      .post(`http://localhost:8081/session/`, {
         sessionName,
         fromTime,
         toTime,
-    } = state;
+      })
+      .then((response) => {
+        console.log(response);
+        //show success alert
+        // alert(`Employee ${response.data.firstName} is Created`);
+        Swal.fire(
+          `Session ${response.data.name} is Added`,
+          "Click Ok to continue",
+          "success"
+        );
+        setTimeout(() => {
+          window.location.reload();
+        }, 1000);
 
-    function handleChange(name) {
-        return function (event) {
-            setState({ ...state, [name]: event.target.value });
-        };
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.table({
-            sessionName,
-            fromTime,
-            toTime,
-            // close,
+        setState({
+          ...state,
+          sessionName: "",
+          fromTime: "",
+          toTime: "",
         });
+      })
+      .catch((error) => {
+        console.log(error);
+        Swal.fire({
+          icon: "error",
+          title: `${error.response.data.error}`,
+          // text: `${error.response.data.error}`,
+          footer: "Please try again",
+        });
+        // alert(error.response.data.error);
+      });
+  };
 
-        axios
-            .post(`http://localhost:8081/session/`, {
-                sessionName,
-                fromTime,
-                toTime,
-                // close,
-            })
-            .then((response) => {
-                console.log(response);
-                //show success alert
-                // alert(`Employee ${response.data.firstName} is Created`);
-                Swal.fire(
-                    `Session ${response.data.name} is Added`,
-                    'Click Ok to continue',
-                    'success'
-                )
-                setTimeout(() => { window.location.reload(); }, 1000);
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
 
-                setState({
-                    ...state,
-                    sessionName: "",
-                    fromTime: "",
-                    toTime: "",
-                });
+      <div
+        className="container "
+        style={{ marginLeft: "90px", position: "absolute" }}
+      >
+        <div
+          className="card scrollable-div"
+          style={{ width: "1240px", height: "590px" }}
+        >
+          <div className="card-body"></div>
+          <h1 align="center">New Session</h1>
+          <br />
 
-            })
-            .catch((error) => {
-                console.log(error);
-                Swal.fire({
-                    icon: 'error',
-                    title: `${error.response.data.error}`,
-                    // text: `${error.response.data.error}`,
-                    footer: 'Please try again'
-                })
-                // alert(error.response.data.error);
-            });
 
-        };
-
-        return (
-            <div>
-                <Navbar />
-                <Sidebar />
-    
-                <div className="container " style={{ marginLeft: "90px", position: "absolute" }}>
-                    <div className="card scrollable-div" style={{ width: "1240px", height: "590px" }}>
-                        <div className="card-body"></div>
-                        <h1 align="center">New Session</h1>
-                        <br />
-
-                        <div className="card">
-                        <div className="card-body">
-                            {/* <center>
+          <div className="card">
+            <div className="card-body">
+              {/* <center>
                                 <div class="row container ">
                                     <div class="col">
                                         <label className="text-muted"> <b>Upload Movie Picture (This is Optional)</b></label><br /><br />
@@ -109,35 +114,55 @@ const AddSession = () => {
                                 </div>
                             </center> */}
 
-                                <br />
-                            <br />
+              <br />
+              <br />
 
-                            <form onSubmit={handleSubmit}>
-                                <div className="form-group">
-                                </div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div className="form-group">
-                                            <label className="text-muted">Session Name</label>
-                                            <input onChange={handleChange('sessionName')} value={sessionName} type="text" className="form-control" placeholder="Enter the Session Name" required />
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="col">
-                                            <div className="form-group">
-                                                <label className="text-muted">From Time</label>
-                                                <input type="text" onChange={handleChange('fromTime')} value={fromTime} className="form-control" placeholder="Enter the session starting time" required />
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                        <div class="col">
-                                            <div className="form-group">
-                                                <label className="text-muted">To Time</label>
-                                                <input type="text" onChange={handleChange('toTime')} value={toTime} className="form-control" placeholder="Enter the session ending time" required />
-                                            </div>
-                                        </div>
-                                        </div>
-                                        {/* <div className="form-group">
+              <form onSubmit={handleSubmit}>
+                <div className="form-group"></div>
+                <div class="row">
+                  <div class="col">
+                    <div className="form-group">
+                      <label className="text-muted">Session Name</label>
+                      <input
+                        onChange={handleChange("sessionName")}
+                        value={sessionName}
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter the Session Name"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div class="col">
+                      <div className="form-group">
+                        <label className="text-muted">From Time</label>
+                        <input
+                          type="text"
+                          onChange={handleChange("fromTime")}
+                          value={fromTime}
+                          className="form-control"
+                          placeholder="Enter the session starting time"
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="col">
+                        <div className="form-group">
+                          <label className="text-muted">To Time</label>
+                          <input
+                            type="text"
+                            onChange={handleChange("toTime")}
+                            value={toTime}
+                            className="form-control"
+                            placeholder="Enter the session ending time"
+                            required
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    {/* <div className="form-group">
                                             <label className="text-muted">Genre</label>
                                             <select id="type" value={genre} onChange={handleChange("genre")} className="form-control">
                                                 <option value="" disabled selected>Select a Genre</option>
@@ -152,15 +177,15 @@ const AddSession = () => {
                                                 <option value="Horror">Horror</option>
                                             </select>
                                         </div> */}
-                                    </div>
-                                </div>
+                  </div>
+                </div>
 
-                                {/* <div className="form-group">
+                {/* <div className="form-group">
                                     <label className="text-muted">Movie Description</label>
                                     <textarea onChange={handleChange("description")} value={description} type="text" className="form-control" placeholder="Enter the Movie Description" pattern="{1,300}" required />
                                 </div> */}
 
-                                {/* <div class="row">
+                {/* <div class="row">
                                     <div class="col">
                                         <div className="form-group">
                                             <label className="text-muted"> Movie Language</label>
@@ -187,13 +212,13 @@ const AddSession = () => {
                                         </div>
                                     </div>
                                 </div> */}
-                                <br />
-                                {/* <div className="form-group">
+                <br />
+                {/* <div className="form-group">
                                     <label className="text-muted">Theater Address</label>
                                     <textarea onChange={handleChange("address")} value={address} type="text" className="form-control" placeholder="Enter the Theater Address" pattern="{1,300}" required />
                                 </div> */}
-                                <br />
-                                {/* <div class="row">
+                <br />
+                {/* <div class="row">
                                     <div class="col">
                                         <div className="form-group">
                                             <label className="text-muted">Close</label>
@@ -204,28 +229,28 @@ const AddSession = () => {
                                             </select>
                                         </div>
                                     </div> */}
-                                    {/* <div class="col">
+                {/* <div class="col">
                                         <div className="form-group">
                                             <label className="text-muted">Number of Seats</label>
                                             <input type="text" onChange={handleChange('noOfSeat')} value={noOfSeat} className="form-control" placeholder="Enter the Release Date" pattern="[0-9]+" title="Please enter only numbers" required />
                                         </div>
                                     </div> */}
-                                {/* </div> */}
-                                <br />
-                                <br />
-                                <div>
-                                    <button className="btn btn-primary btn-lg btn-block">Add Session</button>
-                                </div>
-                                <br />
-                            </form>
-                        </div>
-                    </div>
-
+                {/* </div> */}
+                <br />
+                <br />
+                <div>
+                  <button className="btn btn-primary btn-lg btn-block">
+                    Add Session
+                  </button>
                 </div>
+                <br />
+              </form>
             </div>
+          </div>
         </div>
-    );
-
+      </div>
+    </div>
+  );
 };
 
 export default AddSession;
